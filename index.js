482a18a12da3134199ffb123d53bdf25ddebfad8
victoriovincent <victorio.vincent@yahoo.com>
2021-12-13 11:52:06 +0700

    update typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function reqireHTTPS(req, res, next) {
 
     if (!req.secure && req.get('x-forwarded.proto') !== 'https') {
-        return res.redirect('https//' + req.get('host') + req.url);
+        return res.redirect('https://' + req.get('host') + req.url);
     }
     next();
 }
