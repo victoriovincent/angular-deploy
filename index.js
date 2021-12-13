bb421d8a078df9d20e6468fb2e004469b23efc65
victoriovincent <victorio.vincent@yahoo.com>
2021-12-13 12:07:05 +0700

    update typo2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function reqireHTTPS(req, res, next) {
 
-    if (!req.secure && req.get('x-forwarded.proto') !== 'https') {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
         return res.redirect('https://' + req.get('host') + req.url);
     }
     next();
