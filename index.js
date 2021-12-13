8d65a2c809ced6b304d2158f281e64c257f4f3bc
victoriovincent <victorio.vincent@yahoo.com>
2021-12-13 11:12:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function reqireHTTPS(req, res, next) {
+
+    if (!req.secure && req.get('x-forwarded.proto') !== 'https') {
+        return res.redirect('https//' + req.get('host') + req.url);
+    }
+    next();
+}
+
+const express = require('express');
+const app = express();
+
+app.use(reqireHTTPS);
+app.use(express.static('./dist/test-angular-deployment-test'));
+
+app.get('/*', (req,res) => 
+    res.sendFile('index.html', {root: 'dist/test-angular-deployment-test'}),
+);
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
